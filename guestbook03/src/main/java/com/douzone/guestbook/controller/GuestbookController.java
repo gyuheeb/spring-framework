f6e1b9d9aec8decf34b73927930937cb20b34326
gyuheeb <gyuhee4529@naver.com>
2023-01-30 14:39:49 +0900

    guestbook #01

---
+++ guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -0,0 +1,51 @@
+package com.douzone.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import com.douzone.guestbook.respository.GuestbookRespository;
+import com.douzone.guestbook.vo.GuestbookVo;
+
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestbookRespository guestbookRepository;
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		List<GuestbookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "/WEB-INF/views/list.jsp";
+	}
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "/WEB-INF/views/deleteform.jsp";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
+	public String delete(@PathVariable("no") Long no, String password) {
+		guestbookRepository.deleteByGuest(no,password);
+		return "redirect:/";
+	}
+
+}
