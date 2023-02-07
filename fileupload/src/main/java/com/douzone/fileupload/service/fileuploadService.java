661bfd9032a0f0769bbbeb5134cbd94181d613dd
gyuheeb <gyuhee4529@naver.com>
2023-02-07 11:30:17 +0900

    file upload #1

---
+++ fileupload/src/main/java/com/douzone/fileupload/service/fileuploadService.java
@@ -0,0 +1,12 @@
+package com.douzone.fileupload.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class fileuploadService {
+
+	public static String restore(MultipartFile file) {
+	
+		return null;
+	}
+
+}
