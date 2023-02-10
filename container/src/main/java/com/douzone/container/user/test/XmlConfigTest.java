f8945b634fbae6b3ceebc338f20baef21e442b97
gyuheeb <gyuhee4529@naver.com>
2023-02-10 10:18:53 +0900

    container #2

--- container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -9,12 +9,21 @@ import com.douzone.container.user.User;
 public class XmlConfigTest {
 
 	public static void main(String[] args) {
-		// XML Auto Configuration(Annotation Scanning)
+		// XML Auto Configuration(Annotation Scanning= 자동설정)
 		testBeanFactory01();
+		
+		// XML Bean Configuration(Explicit Configuration= 명시적 설정)
+		testBeanFactory02();
 	}
 
 	private static void testBeanFactory01() {
-		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/applicationContext.xml"));
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/applicationContext01.xml"));
+		User user = bf.getBean(User.class);
+		System.out.println(user.getName());
+	}
+	
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/applicationContext02.xml"));
 		User user = bf.getBean(User.class);
 		System.out.println(user.getName());
 	}
